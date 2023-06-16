8b8797196956c6685803fd8e11473de724aeab6d
Alan <bulgakov.alan@list.ru>
2023-06-16 18:33:34 +0300

    v6

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -2,42 +2,20 @@
   export default {
     data() {
       return {
-        text: 'page',
-        href: 'page.html',
+        num: 5,
+        num1: 1,
+        num2: 2,
+        num3: 3,
       }
     }
   }
 </script>
 
 <template>
-  <a class="inline-link-3" href="page.html">page</a>
+  {{ num * num }}
+  {{ num1 + num2 + num3 }}
 </template>
 
 <style scoped>
-  /* header {
-  line-height: 1.5;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-} */
 </style>
