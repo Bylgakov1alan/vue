4f6b21eff372af9de9438612a0ea9235e6ed3a99
Alan <bulgakov.alan@list.ru>
2023-06-16 18:32:20 +0300

    v5

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -2,18 +2,42 @@
   export default {
     data() {
       return {
-        name: 'main',
-        surname: 'steve',
+        text: 'page',
+        href: 'page.html',
       }
     }
   }
 </script>
 
 <template>
-  <div class="crazy">{{ name }}</div>
-  <div class="crazy">{{ surname }}</div>
+  <a class="inline-link-3" href="page.html">page</a>
 </template>
 
 <style scoped>
+  /* header {
+  line-height: 1.5;
+}
 
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+} */
 </style>
