bad5163047bf84764525395366d0ac746177f1ad
Alan <bulgakov.alan@list.ru>
2023-06-17 10:03:55 +0300

    v11

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,53 +1,32 @@
-.crazy {
-padding: 10px;
-margin: 10px;
-background: #08C;
-color: white;
-font-weight: bold;
-text-decoration: none;
-}
+<script>
+  export default {
+    data() {
+      return {
+        num1: 1,
+        num2: 2,
+        text: 'alec'
+      }
+    },
+    methods: {
+      show: function () {
+        let text = this.cape(this.text)
+        alert(text);
+        // alert(this.num1 + this.num2);
+      },
+      cape: function (str) {
+        return str[0].toUpperCase() + str.slice(1);
+      }
+    }
+  }
+</script>
 
-.crazy:hover {
-background: #0AF
-}
+<template>
+  <div class="crazy">{{ text }}</div>
+  <div class="crazy">{{ num1 + num2 }}</div>
+  <button class="button" v-on:mouseover="show">mouse</button>
+  <button class="button" @click="show">click</button>
+</template>
 
-.button {
-background-color: #852354;
-/* border: none; */
-color: white;
-padding: 15px 32px;
-text-align: center;
-text-decoration: none;
-display: inline-block;
-font-size: 16px;
-margin: 4px 2px;
-cursor: pointer;
-border-radius: 14px;
-}
+<style scoped>
 
-.inline-link-3 {
-display: inline-block;
-position: relative;
-padding-left: 6px;
-font-size: 20px;
-/* Font styles */
-text-decoration: none;
-color: #aa1761;
-text-shadow: 0 1px 1px rgba(255, 255, 255, 0.9);
-}
-
-.inline-link-3:hover {
-color: #3C9CE7;
-}
-
-.inline-link-3:before {
-content: "\25BA";
-font-size: 80%;
-display: inline-block;
-padding-right: 3px;
-pointer-events: none;
-}
-
-.inline-link-3:hover:before {
-color: #0daf28;
-}
+</style>
