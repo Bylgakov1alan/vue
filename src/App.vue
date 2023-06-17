d3c15489ebd6fcc14c42ac1f014e98fe4d676d34
Alan <bulgakov.alan@list.ru>
2023-06-17 11:54:59 +0300

    v57

--- src/App.vue
+++ src/App.vue
@@ -1,57 +1,15 @@
 <script>
+  import crazy from './components/Employee.vue'
+
   export default {
-    data() {
-      return {
-        crazys: [
-          {
-            id: 1,
-            name: 'vizer',
-            salary: 100,
-            age: 19,
-          },
-          {
-            id: 2,
-            name: 'dragon',
-            salary: 200,
-            age: 18,
-          },
-          {
-            id: 3,
-            name: 'steve',
-            salary: 300,
-            age: 25,
-          },
-        ]
-      }
-    },
-    methods: {
-      edit(crazy) {
-        crazy.isEdit = true;
-      },
-      save(crazy) {
-        crazy.isEdit = false;
-      },
+    components: {
+      crazy
     }
   }
 </script>
 
 <template>
-
-  <table class="crazy">
-    <tr v-for="crazy in crazys" :key="crazy.id">
-      <template v-if="!crazy.isEdit">
-        {{ crazy.name }} {{ crazy.salary }} {{ crazy.age }}<button class="button" @click="edit(crazy)">edit</button>
-      </template>
-      <template v-else>
-        <input v-model="crazy.name">
-        <input v-model="crazy.salary">
-        <input v-model="crazy.age">
-        <button class="button" @click="save(crazy)">
-          save
-        </button>
-      </template>
-    </tr>
-  </table>
+  <crazy />
 </template>
 
 <style scoped>
---
+++ src/components/Employee.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+  data() {
+    return {
+      name: "steve",
+    };
+  },
+};
+</script>
+
+<template>
+  <p class="crazy">{{ name }}</p>
+</template>
