af095f1132de14b92249f20580b7a89c7200bd09
Alan <bulgakov.alan@list.ru>
2023-06-17 10:29:50 +0300

    v22

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -2,32 +2,18 @@
   export default {
     data() {
       return {
-        visible: true,
-        visible1: true,
-        visible2: true,
+        iscrazy: true,
       }
     },
     methods: {
-      toggle: function () {
-        this.visible = !this.visible;
-      },
-      toggle1: function () {
-        this.visible1 = !this.visible1;
-      },
-      toggle2: function () {
-        this.visible2 = !this.visible2;
-      }
+
     }
   }
 </script>
 
 <template>
-  <button class="button" @click="toggle">toggle</button>
-  <p class="crazy">{{visible ? 'skelet' : 'ton'}}</p>
-  <button class="button" @click="toggle1">toggle</button>
-  <p class="crazy">{{visible1 ? 'pig' : 'svin'}}</p>
-  <button class="button" @click="toggle2">toggle</button>
-  <p class="crazy">{{visible2 ? 'steve' : 'luzy'}}</p>
+  <p class="crazy" v-if="iscrazy">steve</p>
+  <p class="crazy" v-else>dragon</p>
 </template>
 
 <style scoped>
