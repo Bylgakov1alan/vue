bfa558387f52bfe4c16a10c105517c8aef8d3576
Alan <bulgakov.alan@list.ru>
2023-06-17 11:23:57 +0300

    v37

--- src/App.vue
+++ src/App.vue
@@ -2,49 +2,26 @@
   export default {
     data() {
       return {
-        crazys: ['steve', 'dragon', 'vizer'],
+        cssClass: 'craz',
       }
     },
     methods: {
-      add: function () {
-        this.crazys.push('pig');
-      },
-      del: function () {
-        this.crazys.shift();
-      },
-      del1: function () {
-        this.crazys.pop();
-      },
-      splice: function () {
-        this.crazys.splice(2, 1);
-      },
-      sort: function () {
-        this.crazys.sort();
-      },
-      reverse: function () {
-        this.crazys.reverse();
-      },
+
     }
   }
 </script>
 
 <template>
-  <ul class="styled">
-    <li v-for="elem in crazys">{{ elem }}</li>
-  </ul>
-  <button class="button" @click="add">add crazy</button>
-  <br>
-  <button class="button" @click="del">delete crazy</button>
-  <br>
-  <button class="button" @click="del1">delete crazy at the end</button>
-  <br>
-  <button class="button" @click="splice">delete the penultimate crazy</button>
-  <br>
-  <button class="button" @click="sort">sort array</button>
-  <br>
-  <button class="button" @click="reverse">array in reverse order</button>
+  <p :class="cssClass">crazy</p>
 </template>
 
 <style scoped>
-
+  .craz {
+    padding: 10px;
+    margin: 10px;
+    background: #08C;
+    color: white;
+    font-weight: bold;
+    text-decoration: none;
+  }
 </style>
