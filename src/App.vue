53d5452df58b89ef99e81f38675e70a64bb0b3aa
Alan <bulgakov.alan@list.ru>
2023-06-17 11:04:04 +0300

    v35

--- src/App.vue
+++ src/App.vue
@@ -2,30 +2,47 @@
   export default {
     data() {
       return {
-        products: [
-          {
-            id: 1,
-            name: 'vizer',
-          },
-          {
-            id: 2,
-            name: 'dragon',
-          },
-          {
-            id: 3,
-            name: 'steve',
-          },
-        ]
+        crazys: ['steve', 'dragon', 'vizer'],
       }
     },
     methods: {
-
+      add: function () {
+        this.crazys.push('pig');
+      },
+      del: function () {
+        this.crazys.shift();
+      },
+      del1: function () {
+        this.crazys.pop();
+      },
+      splice: function () {
+        this.crazys.splice(2, 1);
+      },
+      sort: function () {
+        this.crazys.sort();
+      },
+      reverse: function () {
+        this.crazys.reverse();
+      },
     }
   }
 </script>
 
 <template>
-  <p class="crazy" v-for="prod in products" :key='products.id'>{{ prod.name }}</p>
+  <ul class="styled">
+    <li v-for="elem in crazys">{{ elem }}</li>
+  </ul>
+  <button class="button" @click="add">add crazy</button>
+  <br>
+  <button class="button" @click="del">delete crazy</button>
+  <br>
+  <button class="button" @click="del1">delete crazy at the end</button>
+  <br>
+  <button class="button" @click="splice">delete the penultimate crazy</button>
+  <br>
+  <button class="button" @click="sort">sort array</button>
+  <br>
+  <button class="button" @click="reverse">array in reverse order</button>
 </template>
 
 <style scoped>
