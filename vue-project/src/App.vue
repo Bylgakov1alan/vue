5c64826de72752d7ac95657f459a2d19a7e2c15d
Alan <bulgakov.alan@list.ru>
2023-06-17 10:02:43 +0300

    v10

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,20 +1,53 @@
-<script>
-  export default {
-    data() {
+.crazy {
+padding: 10px;
+margin: 10px;
+background: #08C;
+color: white;
+font-weight: bold;
+text-decoration: none;
+}
 
-    },
-    methods: {
-      show: function () {
-        alert(new Date().toLocaleDateString());
-      }
-    }
-  }
-</script>
+.crazy:hover {
+background: #0AF
+}
 
-<template>
-  <button @click="show" class="button">date</button>
-</template>
+.button {
+background-color: #852354;
+/* border: none; */
+color: white;
+padding: 15px 32px;
+text-align: center;
+text-decoration: none;
+display: inline-block;
+font-size: 16px;
+margin: 4px 2px;
+cursor: pointer;
+border-radius: 14px;
+}
 
-<style scoped>
+.inline-link-3 {
+display: inline-block;
+position: relative;
+padding-left: 6px;
+font-size: 20px;
+/* Font styles */
+text-decoration: none;
+color: #aa1761;
+text-shadow: 0 1px 1px rgba(255, 255, 255, 0.9);
+}
 
-</style>
+.inline-link-3:hover {
+color: #3C9CE7;
+}
+
+.inline-link-3:before {
+content: "\25BA";
+font-size: 80%;
+display: inline-block;
+padding-right: 3px;
+pointer-events: none;
+}
+
+.inline-link-3:hover:before {
+color: #0daf28;
+}
