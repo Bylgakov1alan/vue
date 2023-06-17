6afdd9f5b46461a58f6de3b249bc352550384340
Alan <bulgakov.alan@list.ru>
2023-06-17 10:34:49 +0300

    v28

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -2,7 +2,7 @@
   export default {
     data() {
       return {
-        items: [1, 2, 3, 4, 5],
+        arr: ['steve', 'dragon', 'zombie'],
       }
     },
     methods: {
@@ -12,13 +12,7 @@
 </script>
 
 <template>
-  <div class="crazy" v-for="elem in items">{{ elem }}</div>
-  <br>
-  <br>
-  <div class="crazy" v-for="elem in items">{{ elem*elem }}</div>
-  <br>
-  <br>
-  <ul class="crazy" v-for="elem in items">{{ elem }}</ul>
+  <ul class="crazy" v-for="(elem, key) in arr"> {{ key }} {{ elem }}</ul>
 </template>
 
 <style scoped>
