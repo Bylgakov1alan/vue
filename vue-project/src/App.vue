fd9dc2814288a7058adf1d35a00ff62f8808b2fb
Alan <bulgakov.alan@list.ru>
2023-06-17 10:09:15 +0300

    v13

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -2,65 +2,25 @@
   export default {
     data() {
       return {
-        date: '1'
       }
     },
     methods: {
-      show: function () {
-        let date = this.dayOfWeek(this.date);
-        // const dayOfWeekDigit = new Date().getDay();
-        // let days = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье']
-        alert(date);
+      show: function (str) {
+        alert(str * str);
       },
-      dayOfWeek: function (date) {
-        let days = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье']
-        return days[date];
-      },
-      main: function () {
-        const dayOfWeekDigit = new Date().getDay();
-        let days = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье']
-        return dayOfWeekDigit[days]
-      },
-      // dayOfWeek2: function (date) {
-      //   let days = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье']
-      //   return days[date];
-      // }
+      show1: function (str1) {
+        alert(str1 * str1)
+      }
     }
   }
 </script>
 
 <template>
-  <!-- <button v-on:mouseover="show">mouse</button> -->
-  <button class="button" @click="show">click</button>
-  <button class="button" @click="main">click</button>
-  {{ dayOfWeekDigit }}
+  <button class="button" @click="show(2)">btn1</button>
+  <button class="button
+" @click="show1(3)">btn2</button>
 </template>
 
 <style scoped>
-  /* header {
-  line-height: 1.5;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-} */
 </style>
